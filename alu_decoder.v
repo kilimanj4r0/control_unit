9ace3b04a8a6e9a3a4f3af449c67a2bd12a6308f
Vladimir <43704968+kilimanj4r0@users.noreply.github.com>
2020-11-26 17:59:22 +0300

    Control unit
    Control unit and its parts.

---
+++ alu_decoder.v
@@ -0,0 +1,37 @@
+module alu_decoder(
+ 	input [5:0] funct,
+ 	input [1:0] ALUOp,
+ 	output reg [2:0] ALUControl
+);
+
+always@ (*) begin
+ 	case (ALUOp) 
+ 	2'b00 : begin
+ 		ALUControl = 3'b010;
+ 	end
+ 	2'b01 : begin
+ 		ALUControl = 3'b110;
+ 	end
+ 	2'b10 : begin
+  		case (funct)
+  		6'h20 : begin
+  			ALUControl = 3'b010;
+  		end
+  		6'h22 : begin
+  			ALUControl = 3'b110;
+  		end
+  		6'h24 : begin
+  			ALUControl = 3'b000;
+  		end
+  		6'h25 : begin
+  			ALUControl = 3'b001;
+  		end
+  		6'h2A : begin
+  			ALUControl = 3'b111;
+  		end
+		endcase
+ 	end
+	endcase
+end
+
+endmodule
